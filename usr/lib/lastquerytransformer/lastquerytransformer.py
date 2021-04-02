acac1cb2c6bb87d024f38f87be865928e0c01125
pierre-si <62605527+pierre-si@users.noreply.github.com>
2021-04-02 21:31:52 +0200

    extract model and dataset into two modules

---
+++ usr/lib/lastquerytransformer/lastquerytransformer.py
@@ -0,0 +1,125 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+    
+class RiiidEmbedding(nn.Module):
+    def __init__(self, maximums, pad_idx=0, emb_size=16, dim=128):
+        super().__init__()
+        self.emb_size = emb_size
+        self.question_emb = nn.Embedding(maximums['question_id']+1, emb_size, padding_idx = pad_idx)
+        self.part_emb = nn.Embedding(maximums['part']+1, emb_size, padding_idx = pad_idx)
+        self.answer_emb = nn.Embedding(maximums['answered_correctly']+1, emb_size, padding_idx=pad_idx)
+        self.cont_emb = nn.Sequential(
+            nn.Linear(2, emb_size),
+            nn.LayerNorm(emb_size)
+            )
+        self.merge = nn.Linear(4*emb_size, dim)#, bias=False)
+
+    def forward(self, x_cat, x_cont):
+        if len(x_cat.size()) == 2:
+            x_cat = x_cat.unsqueeze(0)
+            x_cont = x_cont.unsqueeze(0)
+        q_emb = self.question_emb(x_cat[:, :, 0])
+        p_emb = self.part_emb(x_cat[:, :, 1])
+        a_emb = self.answer_emb(x_cat[:, :, 2])
+        cont_emb = self.cont_emb(x_cont)
+        emb = torch.cat([q_emb, p_emb, a_emb, cont_emb], dim=2)
+        emb = self.merge(emb)
+        return emb
+
+def _get_activation_fn(activation):
+    if activation == "relu":
+        return F.relu
+    elif activation == "gelu":
+        return F.gelu
+
+class LastQueryTransformerEncoderLayer(nn.Module):
+    r"""TransformerEncoderLayer is made up of self-attn and feedforward network.
+    This standard encoder layer is based on the paper "Attention Is All You Need".
+    Ashish Vaswani, Noam Shazeer, Niki Parmar, Jakob Uszkoreit, Llion Jones, Aidan N Gomez,
+    Lukasz Kaiser, and Illia Polosukhin. 2017. Attention is all you need. In Advances in
+    Neural Information Processing Systems, pages 6000-6010. Users may modify or implement
+    in a different way during application.
+
+    Args:
+        d_model: the number of expected features in the input (required).
+        nhead: the number of heads in the multiheadattention models (required).
+        dim_feedforward: the dimension of the feedforward network model (default=2048).
+        dropout: the dropout value (default=0.1).
+        activation: the activation function of intermediate layer, relu or gelu (default=relu).
+
+    Examples::
+        >>> encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=8)
+        >>> src = torch.rand(10, 32, 512)
+        >>> out = encoder_layer(src)
+    """
+
+    def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1, activation="relu"):
+        super(LastQueryTransformerEncoderLayer, self).__init__()
+        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
+        # Implementation of Feedforward model
+        self.linear1 = nn.Linear(d_model, dim_feedforward)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(dim_feedforward, d_model)
+
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+        self.activation = _get_activation_fn(activation)
+
+    def __setstate__(self, state):
+        if 'activation' not in state:
+            state['activation'] = F.relu
+        super(LastQueryTransformerEncoderLayer, self).__setstate__(state)
+
+    def forward(self, src, src_mask = None, src_key_padding_mask = None):
+        r"""Pass the input through the encoder layer.
+
+        Args:
+            src: the sequence to the encoder layer (required).
+            src_mask: the mask for the src sequence (optional).
+            src_key_padding_mask: the mask for the src keys per batch (optional).
+
+        Shape:
+            see the docs in Transformer class.
+        """
+        src2 = self.self_attn(src[-1:, :, :], src, src, attn_mask=src_mask,
+                              key_padding_mask=src_key_padding_mask)[0]
+        src = src + self.dropout1(src2) # scr2's second dimension is broadcasted
+        src = self.norm1(src)
+        src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))
+        src = src + self.dropout2(src2)
+        src = self.norm2(src)
+        return src
+
+class Riiid(nn.Module):
+    def __init__(self, maximums, pad_idx = 0):
+        super(Riiid, self).__init__()
+        self.pad_idx = pad_idx
+        self.emb = RiiidEmbedding(maximums, pad_idx=pad_idx)
+        self.encoder_layer = LastQueryTransformerEncoderLayer(d_model=128, nhead=8)
+        self.lstm = nn.LSTM(input_size=128, hidden_size=128, batch_first=True)
+        self.dnn = nn.Sequential(
+            nn.Linear(128, 256),
+            nn.ReLU(),
+            #nn.BatchNorm1d(256),
+            nn.Linear(256, 1),
+        )
+                
+    def forward(self, x_cat, x_cont):
+        pad_mask = self.make_padding_mask(x_cat).transpose(1, 0)
+        x = self.emb(x_cat, x_cont)
+
+        x = x.transpose(1, 0) # pytorch MHA requires input to be S×N×E
+        x = self.encoder_layer(x, src_key_padding_mask=pad_mask)
+        x = x.transpose(1, 0)
+        x = self.lstm(x)[1][0]
+        x = self.dnn(x).squeeze(1)
+        
+        return x 
+
+    def make_padding_mask(self, x_cat):
+        pad_mask = (x_cat[:, :, 0] == self.pad_idx).transpose(1, 0)
+        return pad_mask
---
+++ usr/lib/riiidutils/riiidutils.py
@@ -0,0 +1,43 @@
+import torch
+
+class RiiidDataset(torch.utils.data.Dataset):
+    def __init__(self, x_cat, x_cont, y):
+        seq_lengths = [len(el) for el in x_cat]
+        _, self.cat, self.cont, self.y = map(list, zip(*sorted(zip(seq_lengths, x_cat, x_cont, y), key=lambda tup: tup[0], reverse=True)))
+        #map(list, zip(*sorted(zip(x_cat, x_cont, y), key= lambda i: len(i[0]), reverse=True))) # does not seem to work
+
+    def __getitem__(self, key):
+        #return {'cat': self.cat[key], 'cont': self.cont[key], 'y': self.y[key]}
+        return self.cat[key], self.cont[key], self.y[key]
+        
+    def __len__(self):
+        return len(self.cat)
+
+def riiid_collate_fn(batch):
+    cat, cont, y = zip(*batch)
+    cat = pad_sequence_left([torch.tensor(el) for el in cat], batch_first=True)
+    cont = pad_sequence_left([torch.tensor(el, dtype=torch.float) for el in cont], batch_first=True)
+    y = torch.tensor(y, dtype=torch.float).unsqueeze(1)
+    return {'cat': cat, 'cont': cont, 'y': y}
+
+def pad_sequence_left(sequences, batch_first=False, padding_value=0.0):
+    r"""Pad a list of variable length Tensors with ``padding_value`` to the left.
+    Copied from rnn.pad_sequence"""
+    max_size = sequences[0].size()
+    trailing_dims = max_size[1:]
+    max_len = max([s.size(0) for s in sequences])
+    if batch_first:
+        out_dims = (len(sequences), max_len) + trailing_dims
+    else:
+        out_dims = (max_len, len(sequences)) + trailing_dims
+
+    out_tensor = sequences[0].new_full(out_dims, padding_value)
+    for i, tensor in enumerate(sequences):
+        length = tensor.size(0)
+        # use index notation to prevent duplicate references to the tensor
+        if batch_first:
+            out_tensor[i, max_len-length:, ...] = tensor
+        else:
+            out_tensor[max_len-length:, i, ...] = tensor
+
+    return out_tensor
