d5a743771e4a48641d3b9a45b853234a84227e3f
pierre-si <62605527+pierre-si@users.noreply.github.com>
2021-04-24 12:56:47 +0200

    add positional encoding

--- usr/lib/lastquerytransformer/lastquerytransformer.py
+++ usr/lib/lastquerytransformer/lastquerytransformer.py
@@ -1,3 +1,5 @@
+import math
+
 import torch
 from torch import nn
 import torch.nn.functional as F
@@ -33,6 +35,24 @@ def _get_activation_fn(activation):
         return F.relu
     elif activation == "gelu":
         return F.gelu
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
 
 class LastQueryTransformerEncoderLayer(nn.Module):
     r"""TransformerEncoderLayer is made up of self-attn and feedforward network.
@@ -99,7 +119,9 @@ class Riiid(nn.Module):
     def __init__(self, maximums, pad_idx = 0, dropout = 0.1):
         super(Riiid, self).__init__()
         self.pad_idx = pad_idx
-        self.emb = RiiidEmbedding(maximums, pad_idx=pad_idx)
+        self.emb = RiiidEmbedding(maximums, pad_idx=pad_idx, dim=128)
+        self.pos_encoder = PositionalEncoding(d_model=128, dropout=dropout, max_len=16409)
+        
         self.encoder_layer = LastQueryTransformerEncoderLayer(d_model=128, nhead=8, dim_feedforward=256, dropout=dropout)
         self.lstm = nn.LSTM(input_size=128, hidden_size=128, batch_first=False) # batch_first is False by default.
         self.dnn = nn.Sequential(
@@ -112,7 +134,7 @@ class Riiid(nn.Module):
     def forward(self, x_cat, x_cont):
         pad_mask = self.make_padding_mask(x_cat)
         x = self.emb(x_cat, x_cont)
-
+        x = self.pos_encoder(x)
         x = x.transpose(1, 0) # pytorch MHA requires input to be S×N×E
         x = self.encoder_layer(x, src_key_padding_mask=pad_mask)
         x = self.lstm(x)[1][0] # h_n: n_layers*n_directions (=1) × batch × hidden_size
